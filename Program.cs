581b0d72df0f115d59ec8170a2c6ad31e5854244
Ahmet Özgür Darendeli <ahmetozgurdarendeli@unknown4ef178fc117d.home>
2025-08-23 16:09:17 +0300

    added

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-Env.Load();
 
+
+// Add services to the container.
+DotNetEnv.Env.Load(Path.Combine(AppContext.BaseDirectory, ".env"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -24,11 +25,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 // app.UseHttpsRedirection();
 
